4f21ce9a437fee6a647484b3c825544a5db0ce0d
Wizark <arbuenop@gmail.com>
2022-04-14 19:08:17 +0200

    0.0.4 - loader
    - spinner loader added up

--- TimiProject/src/app/shared/services/auth.service.ts
+++ TimiProject/src/app/shared/services/auth.service.ts
@@ -7,11 +7,16 @@ import {
   AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   userData: any; // Save logged in user data
+
+  private loading: BehaviorSubject<boolean>;
+
   constructor(
     public afs: AngularFirestore, // Inject Firestore service
     public afAuth: AngularFireAuth, // Inject Firebase auth service
@@ -30,45 +35,58 @@ export class AuthService {
         JSON.parse(localStorage.getItem('user')!);
       }
     });
+    this.loading = new BehaviorSubject<boolean>(false);
   }
   // Sign in with email/password
   SignIn(email: string, password: string) {
+
+    this.setStateLoading(true)
     return this.afAuth
       .signInWithEmailAndPassword(email, password)
       .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
         this.SetUserData(result.user);
       })
       .catch((error) => {
         window.alert(error.message);
+      })
+      .finally(() => {
+        this.setStateLoading(false)
+        this.goDashboard()
       });
   }
   // Sign up with email/password
-  SignUp(email: string, password: string) {
-    return this.afAuth
+  async SignUp(email: string, password: string) {
+    this.setStateLoading(true)
+    return await this.afAuth
       .createUserWithEmailAndPassword(email, password)
       .then((result) => {
         /* Call the SendVerificaitonMail() function when new user sign
         up and returns promise */
-        this.SendVerificationMail();
         this.SetUserData(result.user);
       })
       .catch((error) => {
         window.alert(error.message);
+      })
+      .finally(() => {
+        this.SendVerificationMail();
+        this.setStateLoading(false)
       });
   }
   // Send email verfificaiton when new user sign up
   SendVerificationMail() {
+    this.setStateLoading(true);
     return this.afAuth.currentUser
       .then((u: any) => u.sendEmailVerification())
       .then(() => {
         this.router.navigate(['/auth/verify-email-address']);
+      })
+      .finally(() => {
+        this.setStateLoading(false);
       });
   }
   // Reset Forggot password
   ForgotPassword(passwordResetEmail: string) {
+    this.setStateLoading(true)
     return this.afAuth
       .sendPasswordResetEmail(passwordResetEmail)
       .then(() => {
@@ -76,6 +94,9 @@ export class AuthService {
       })
       .catch((error) => {
         window.alert(error);
+      })
+      .finally(() => {
+        this.setStateLoading(false)
       });
   }
   // Returns true when user is looged in and email is verified
@@ -84,26 +105,37 @@ export class AuthService {
     return user !== null && user.emailVerified !== false ? true : false;
   }
   // Sign in with Google
-  GoogleAuth() {
-    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      if (res) {
-        this.router.navigate(['dashboard']);
-      }
+  async GoogleAuth() {
+    this.setStateLoading(true);
+    return await this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
+      // if (res) {
+      //   this.goDashboard()
+      // }
+    })
+    .catch((error) => {
+      window.alert(error);
+    })
+    .finally(() => {
+      this.setStateLoading(false);
+      this.goDashboard()
     });
   }
   // Auth logic to run auth providers
   AuthLogin(provider: any) {
+    this.setStateLoading(true)
     return this.afAuth
       .signInWithPopup(provider)
       .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
         this.SetUserData(result.user);
       })
       .catch((error) => {
         window.alert(error);
-      });
+      })
+      .finally(() => {
+        this.setStateLoading(false);
+        this.goDashboard()
+      })
+      ;
   }
   /* Setting up user data when sign in with username/password,
   sign up with username/password and sign in with social auth
@@ -127,10 +159,24 @@ export class AuthService {
   SignOut() {
     return this.afAuth.signOut().then(() => {
       localStorage.removeItem('user');
-      this.router.navigate(['/auth/sign-in']);
+      this.router.navigate(['/auth/init']);
     });
   }
 
+  isLoading(): Observable<boolean>  {
+    return this.loading.asObservable();
+  }
 
+  setStateLoading(is:boolean): void {
+    this.loading.next(is);
+  }
+
+  goDashboard() {
+    this.ngZone.run(() => {
+      this.router.navigate(['/dashboard']);
+    });
+
+    this.router.navigate(['/dashboard']);
+  }
 
 }
